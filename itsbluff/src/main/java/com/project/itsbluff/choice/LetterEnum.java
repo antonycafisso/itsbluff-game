6ebb5e258aaf4a474d04cf60c1a2b499f01b3107
Antony Cafisso <tonycafisso@gmail.com>
2024-06-15 16:53:41 -0300

    Create method to return a random initial letter

--- itsbluff/src/main/java/com/project/itsbluff/choice/LetterEnum.java
+++ itsbluff/src/main/java/com/project/itsbluff/choice/LetterEnum.java
@@ -2,4 +2,8 @@ package com.project.itsbluff.choice;
 
 public enum LetterEnum {
     A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+
+    public static LetterEnum getRandomLetter() {
+        return values()[(int) (Math.random() * values().length)];
+    }
 }
